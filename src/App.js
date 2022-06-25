48ba1916222c27d7e3fba74aea7e99f6e8a1164c
muhammadniam <zeronejapanniam3@gmail.com>
2022-06-26 00:56:19 +0700

    feat: create simple component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
+import Contact from './components/Contact';
+import Header from './components/Header';
 
 // Uncomment untuk memuat daftar kontak
-// import contacts from './data/contacts.json';
+import contacts from './data/contacts.json';
 
 const App = () => {
   // Masukkan Header dan Contact ke dalam div App
   return (
     <div className="App">
-      
+      <Header />
+      {
+        contacts.map((data,key) => {
+         return <Contact key={key} data={data} />
+        })
+      }
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,25 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
+import './Contact.css';
 
-const Contact = () => {
+const Contact = ({ data }) => {
     return (
-        <></>
+        <>
+            <div className='card'>
+                <div className='left'>
+                    <img src={data.photo} alt='{props.name}'></img>
+                </div>   
+                <div className='right'>
+                    <h4><b>{data.name}</b></h4>
+                    <p>{data.phone}</p>
+                    <p>{ data.email }</p>
+                </div>
+            </div>     
+        </>
+                 
     )
 }
+
 
 export default Contact;
